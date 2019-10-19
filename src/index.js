78a10820f132d14fa10044ead5755c1016347ec3
Jair <jaircelisv@gmail.com>
2019-10-19 11:12:39 -0500

    Primer problema && Segundo Problema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express"),
   path = require("path"),
   app = express(),
-  port = process.env.PORT || 3000;
+  { config } = require('../config/index');
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
@@ -9,8 +9,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
@@ -18,10 +18,10 @@ app.get('/products', (req, res) => {
   res.json(storeProducts);
 });
 
-app.listen(port, err => {
+app.listen(config.port, err => {
   if (err) {
     console.error("Error: ", err);
     return;
   }
-  console.log(`Listening http://localhost:${port}`);
+  console.log(`listening http://localhost:${config.port}`); 
 });
